bd360146bf93440a1074a2deeaae1a32382f6977
julihanD <david.jgarcia@udea.edu.co>
2025-07-12 20:25:20 -0500

    se corrigen ciertos bugs en el juego

--- Proyecto_final/barravida.cpp
+++ Proyecto_final/barravida.cpp
@@ -1,22 +1,22 @@
-#include <QGraphicsRectItem>
+#include "barravida.h"
 #include <QBrush>
 #include <QPen>
 
-
-class BarraVida : public QGraphicsRectItem
+BarraVida::BarraVida(QGraphicsItem* parent)
+    : QGraphicsRectItem(parent)
 {
-public:
-    BarraVida(QGraphicsItem* parent = nullptr)
-        : QGraphicsRectItem(parent)
-    {
-        setRect(0, 0, 100, 10);  // Tamaño inicial
-        setBrush(QBrush(Qt::red));
-        setPen(Qt::NoPen);
-    }
+    setRect(0, 0, 100, 10);  // Tamaño inicial
+    setBrush(QBrush(Qt::red));
+    setPen(Qt::NoPen);
+}
 
-    void actualizarVida(int vidaActual, int vidaMaxima)
-    {
-        qreal porcentaje = qreal(vidaActual) / vidaMaxima;
-        setRect(0, 0, 100 * porcentaje, 10);
-    }
-};
+void BarraVida::actualizarVida(int vidaActual, int vidaMaxima)
+{
+    if (vidaMaxima <= 0) return;
+
+    qreal porcentaje = (vidaActual * 1.0) / vidaMaxima;
+    if (porcentaje < 0) porcentaje = 0;
+    if (porcentaje > 1) porcentaje = 1;
+
+    setRect(0, 0, 100 * porcentaje, 10);
+}
--- Proyecto_final/barravida.h
+++ Proyecto_final/barravida.h
@@ -2,30 +2,13 @@
 #define BARRAVIDA_H
 
 #include <QGraphicsRectItem>
-#include <QBrush>
-#include <QPen>
 
 class BarraVida : public QGraphicsRectItem
 {
 public:
-    explicit BarraVida(QGraphicsItem* parent = nullptr)
-        : QGraphicsRectItem(parent)
-    {
-        setRect(0, 0, 100, 10);  // Tamaño inicial
-        setBrush(QBrush(Qt::red));
-        setPen(Qt::NoPen);
-    }
+    BarraVida(QGraphicsItem* parent = nullptr);
 
-    void actualizarVida(int vidaActual, int vidaMaxima)
-    {
-        if (vidaMaxima <= 0) return;
-
-        qreal porcentaje = static_cast<qreal>(vidaActual) / vidaMaxima;
-        if (porcentaje < 0) porcentaje = 0;
-        if (porcentaje > 1) porcentaje = 1;
-
-        setRect(0, 0, 100 * porcentaje, 10);
-    }
+    void actualizarVida(int vidaActual, int vidaMaxima);
 };
 
 #endif // BARRAVIDA_H
